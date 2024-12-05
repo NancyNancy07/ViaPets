8f5e9422cee024d0783fda944faab954b10aefa4
Kamin1995 <rahmanykamin@gmail.com>
2024-12-05 10:36:25 +0100

    added pet classes

---
+++ pets/src/Fish.java
@@ -0,0 +1,58 @@
+public class Fish extends Pet
+{
+  private boolean isPredator;
+  private boolean isFreshWater;
+
+  public Fish(String species, int age, String gender, String color, String name,
+      String comment, double price, boolean isPredator, boolean isFreshWater)
+  {
+    super(species, age, gender, color, name, comment, price);
+    this.isFreshWater = isFreshWater;
+    this.isPredator = isPredator;
+  }
+
+  public Fish(String species, int age, String gender, String color,
+      String comment, double price, boolean isPredator, boolean isFreshWater)
+  {
+    super(species, age, gender, color, comment, price);
+    this.isFreshWater = isFreshWater;
+    this.isPredator = isPredator;
+  }
+
+  public boolean isFreshWater()
+  {
+    return isFreshWater;
+  }
+
+  public void setFreshWater(boolean freshWater)
+  {
+    isFreshWater = freshWater;
+  }
+
+  public boolean isPredator()
+  {
+    return isPredator;
+  }
+
+  public void setPredator(boolean predator)
+  {
+    this.isPredator = isPredator;
+  }
+
+  public String toString()
+  {
+    return super.toString() + ", isFreshWater=" + isFreshWater + ", Predator='"
+        + isPredator + '\'';
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (obj == null || getClass() != obj.getClass())
+    {
+      return false;
+    }
+    Fish other = (Fish) obj;
+    return super.equals(other) && isPredator == other.isPredator
+        && isFreshWater == other.isFreshWater;
+  }
+}
---
+++ pets/src/Pet.java
@@ -0,0 +1,114 @@
+public class Pet
+{
+  private String species;
+  private int age;
+  private String gender;
+  private String color;
+  private String name;
+  private String comment;
+  private double price;
+
+  public Pet(String species, int age, String gender, String color, String name,
+      String comment, double price)
+  {
+    this.species = species;
+    this.age = age;
+    this.gender = gender;
+    this.color = color;
+    this.name = name;
+    this.comment = comment;
+    this.price = price;
+  }
+
+  public Pet(String species, int age, String gender, String color,
+      String comment, double price)
+  {
+    this.species = species;
+    this.age = age;
+    this.gender = gender;
+    this.color = color;
+    this.comment = comment;
+    this.price = price;
+  }
+
+  public String getSpecies()
+  {
+    return species;
+  }
+
+  public int getAge()
+  {
+    return age;
+  }
+
+  public String getColor()
+  {
+    return color;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getComment()
+  {
+    return comment;
+  }
+
+  public double getPrice()
+  {
+    return price;
+  }
+
+  public String getGender()
+  {
+    return gender;
+  }
+
+  public void setSpecies(String species)
+  {
+    this.species = species;
+  }
+
+  public void setAge(int age)
+  {
+    this.age = age;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setComment(String comment)
+  {
+    this.comment = comment;
+  }
+
+  public void setPrice(double price)
+  {
+    this.price = price;
+  }
+
+  public String toString()
+  {
+    return "Pet{" + "species='" + species + '\'' + ", age=" + age + ", gender='"
+        + gender + '\'' + ", color='" + color + '\'' + ", name='" + name + '\''
+        + ", comment='" + comment + '\'' + ", price=" + price + '}';
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (obj == null || getClass() != obj.getClass())
+    {
+      return false;
+    }
+
+    Pet other = (Pet) obj;
+    return species.equals(other.species) && age == other.age && gender.equals(
+        other.gender) && color.equals(other.color) && name.equals(other.name)
+        && comment.equals(other.comment) && price == other.price;
+  }
+
+}
