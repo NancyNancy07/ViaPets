3ed3977958ea2c56e8ada9f4559dbf8a28bf7e4f
Kamin1995 <rahmanykamin@gmail.com>
2024-12-16 11:00:07 +0100

    Remove Pet constructor

--- ViaPets/src/model/Pet.java
+++ ViaPets/src/model/Pet.java
@@ -57,19 +57,7 @@ public class Pet implements Serializable
     this.color = color;
     this.comment = comment;
     this.price = price;
-  }
-
-  /**
-   * A No-argument constructor initializing the Pet.
-   */
-  public Pet()
-  {
-    this.species = "";
-    this.age = -1;
-    this.gender = "";
-    this.color = "";
-    this.comment = "";
-    this.price = -0.0;
+    this.name = "";
   }
 
   /**
